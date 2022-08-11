bf9590b4291643478f38bf247964755e2d872d05
Ibsiany Dias <69012623+Ibsiany@users.noreply.github.com>
2022-08-11 00:34:23 +0000

    Adicionando exceções

--- Graph.java
+++ Graph.java
@@ -10,6 +10,12 @@ public class Graph {
   }
 
   public void addEdge(int source, int sink, int weight) {
+    if (source < 0 || source >= this.adjMatrix.length || sink < 0 || sink >= this.adjMatrix.length || weight <= 0) {
+      System.err.println("Invalid edge!\nsource: " + source + "\nsink: " + sink + "\nweight: " + weight);
+
+      return;
+    }
+
     this.countEdges++;
     this.adjMatrix[source][sink] = weight;
   }
--- Main.java
+++ Main.java
@@ -5,6 +5,10 @@ class Main {
     System.out.println(g1);
 
     g1.addEdge(0, 1, 3);
+    g1.addEdge(1, 0, 3);
+    g1.addEdge(0, 3, 4);
+    g1.addEdge(3, 0, 4);
+    g1.addEdge(0, 4, 1);
 
     System.out.println(g1);
   }
