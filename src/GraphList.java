cc19c15c16a1a94e8e53b5a1e47877e6c5f19c6e
Ibsiany Dias <ibsianyd@gmail.com>
2022-10-21 21:15:31 -0300

    Update GraphList.java

--- src/GraphList.java
+++ src/GraphList.java
@@ -138,22 +138,27 @@ public class GraphList {
         return true;
     }
 
-    public ArrayList<Integer> bfs(int s) {
-        // TDOO tarefa da parte 1
-        return new ArrayList<>();
-    }
-
-    public ArrayList<Integer> dfs(int s) {
-        // TDOO tarefa da parte 1
-        return new ArrayList<>();
-    }
-
    public boolean connected() {
         return this.bfs(0).size() == this.countNodes;
     }
 
     public boolean isOriented() {
-        // TDOO tarefa da parte 1
+       for (int u = 0; u < this.adjList.size(); ++u) {
+             for (int idx = 0; idx < this.adjList.get(u).size(); ++idx) {
+                 int v = this.adjList.get(u).get(idx).getSink();
+                 boolean hasEdgeVU = false;
+                 for (int idx2 = 0; idx2 < this.adjList.get(v).size(); ++idx2) {
+                     int u2 = this.adjList.get(v).get(idx2).getSink();
+                     if (u == u2) {
+                         hasEdgeVU = true;
+                         break;
+                     }
+                 }
+                 if (!hasEdgeVU) {
+                     return true;
+                 }
+             }
+         }
         return false;
     }
 
